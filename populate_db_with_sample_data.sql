07dec83b07dd5ac7911082f219c793534a35828b
Saman Sohrab <ssohrab@blackberry.com>
2014-03-14 15:16:12 -0400

    Adding a script for populating sample data

---
+++ populate_db_with_sample_data.sql
@@ -0,0 +1,4 @@
+SELECT * FROM obj_users;
+
+INSERT INTO obj_users (first_name, last_name, email_address, phone_number, email_verification_status, password)
+VALUES ('Saman', 'Sohrab', 'ssohrab@gmail.com', '999-999-9999', 1, 'blahencodedpassword');
